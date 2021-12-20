8b10ae500ba3bf3f01b212dbe375c8f42a9bc8d5
yamadaitsuki <yamada351035@gmail.com>
2021-12-20 23:03:56 +0900

    Pre-rendering and Data Fetching
    https://nextjs.org/learn/basics/assets-metadata-css

---
+++ nextjs-blog/lib/posts.js
@@ -0,0 +1,37 @@
+import * as fs from 'fs';
+import matter from 'gray-matter';
+import * as path from 'path';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // Get files names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map(fileName => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/¥.md$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort(({date: a}, {date: b}) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
