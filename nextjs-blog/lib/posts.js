9fdd71effe41ea3bcdfaaa18416130be020765ce
yamadaitsuki <yamada351035@gmail.com>
2021-12-31 11:54:53 +0900

    Dynamic Routes

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 import matter from 'gray-matter';
 import * as path from 'path';
+import {remark} from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -34,4 +36,36 @@ export function getSortedPostsData() {
       return 0;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
+
+export async function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark().
+      use(html).
+      process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data,
+  };
 }
