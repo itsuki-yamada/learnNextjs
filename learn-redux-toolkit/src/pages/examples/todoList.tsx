6097ccda27e8b71263c29b06e189fe3ec86d9e01
yamadaitsuki <yamada351035@gmail.com>
2022-01-12 23:44:34 +0900

    Todoリスト表示仮コミット
    learnNextjs-#4 状態管理ライブラリについて調査する

--- learn-redux-toolkit/src/pages/examples/todoList.tsx
+++ learn-redux-toolkit/src/pages/examples/todoList.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+type Todo = {
+  title: string,
+  tag: string[],
+  createdAt: string
+}
+
 export default function TodoList() {
-    return (
-        <ul>
-            <li>hoge</li>
-        </ul>
-    );
+  // TODO: Todoのリストを取得する（とりあえずhooksで実装する)
+  // TODO: 更新処理を追加する
+  // TODO: Redux-Toolkitに書き換える
+  const todos: [Todo] = [
+    {
+      title: '内見',
+      tag: ['引越し'],
+      createdAt: '2022/01/12'
+    }
+  ]
+  return (
+    <>
+      <h1>TodoList</h1>
+      <ul>
+        {
+          todos.map((todo: Todo, index: number) => (
+            <li key={index}>{todo.title}</li>
+          ))
+        }
+      </ul>
+    </>
+  );
 }
