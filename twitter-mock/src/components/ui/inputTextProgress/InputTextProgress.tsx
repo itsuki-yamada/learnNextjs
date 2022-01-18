e2af93e1acf457d1044deeb2adfa25dc72f678ea
yamadaitsuki <yamada351035@gmail.com>
2022-01-18 22:26:53 +0900

    レイアウト修正
    learnNextjs-5 TwitterMockアプリを作ってみる

--- twitter-mock/src/components/ui/inputTextProgress/InputTextProgress.tsx
+++ twitter-mock/src/components/ui/inputTextProgress/InputTextProgress.tsx
@@ -1,36 +1,68 @@
-import { VFC } from "react";
+import React, { VFC } from "react";
 import { Box } from "@mui/system";
 import { CircularProgress, Typography } from "@mui/material";
 
 type props = {
   limit: number,
-  currentLength: number
+  currentLength: number,
+  size?: number
 }
 
-// TODO: [] 別ファイル管理に移行する
-// TODO: [] プログレスのwidthを調整して文字が中心に来るように変更する
-export const InputTextProgress: VFC<props> = ({ limit, currentLength }) => {
-  return (
-    <>
-      <Box sx={{ position: 'relative', display: 'inline-flex', backgroundColor: 'skyblue' }}>
-        <CircularProgress variant={'determinate'} value={20} style={{backgroundColor: 'lemonchiffon'}}/>
-        <Box
-          sx={{
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography variant="caption" component="div" color="text.secondary">
-            {`${Math.round(10)}`}
-          </Typography>
+// TODO: cssを別ファイル管理に変更する
+export const InputTextProgress: VFC<props> = React.memo(
+  function InputTextProgress({ limit, currentLength, size = 40 }) {
+    const per = Math.round(currentLength / limit * 100);
+    const isError = per > 100;
+    return (
+      <>
+        <Box sx={{
+          position: 'relative',
+          display: 'inline-flex',
+          backgroundColor: 'skyblue',
+          height: `${size}px`,
+          width: `${size}px`
+        }}>
+          <CircularProgress
+            variant={'determinate'}
+            value={per > 100 ? 100 : per}
+            color={isError ? 'error' : 'primary'}
+            size='100%'
+            style={{
+              backgroundColor: 'lemonchiffon',
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              position: 'absolute',
+            }}
+          />
+          <Box
+            sx={{
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              height: `100%`,
+              position: 'absolute',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {isError && (
+              <Typography
+                variant="caption"
+                component="div"
+                color="error"
+              >
+                {`${limit - currentLength}`}
+              </Typography>
+            )}
+          </Box>
         </Box>
-      </Box>
-    </>
-  );
-};
+      </>
+    );
+  }, (prev, next) => (
+    prev.limit === next.limit &&
+    prev.currentLength === next.currentLength
+  ));
